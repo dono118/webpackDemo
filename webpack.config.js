87b28e465332ffe4bbbc6a8ecb35578450135645
dono118 <dono118@126.com>
2020-08-15 09:08:18 +0800

    使用devServer

--- webpack.config.js
+++ webpack.config.js
@@ -104,4 +104,19 @@ module.exports = {
     // 模式
     mode: 'development', //开发模式
     // mode: 'production'
+
+    // 开发服务器 devServer: 用来自动化 (自动编译, 自动打开浏览器, 自动刷新浏览器)
+    // 特点: 只会在内存中编译打包, 不会有任何输出
+    // 启动devServer指令: npx webpack-dev-server (需下载该插件)
+    devServer: {
+        // 项目构建后的路径
+        contentBase: resolve(__dirname, 'build'),
+        // 启动gzip压缩
+        compress: true,
+        // 端口号
+        port: 3000,
+        // 自动打开浏览器
+        open: true
+    }
+
 }
