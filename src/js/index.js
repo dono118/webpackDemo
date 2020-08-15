9b27d48a48d4da54519dfb0a675d9b82da2b9cb0
dono118 <dono118@126.com>
2020-08-15 16:22:40 +0800

    css兼容性处理

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,2 @@
 // 引入样式文件
-import '../css/iconfont.css';
-import '../css/index.less';
-
-function add(x, y) {
-    return x + y;
-}
-
-console.log(add(1, 2));
+import '../css/index.css';
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@
 const { resolve } = require("path");
 // HtmlWebpackPlugin 打包html文件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// 提取css成单独文件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// 设置nodejs环境变量
+process.env.NODE_ENV = "development";
 
 module.exports = {
     // webpack配置
@@ -34,9 +39,43 @@ module.exports = {
                 use: [
                     // use 数组中loader执行顺序: 先执行 css-loader 后执行 style-loader
                     // 创建style标签, 将js中的样式资源放进去, 并添加到head中生效
-                    'style-loader',
+                    // 'style-loader',
+                    // 这个loader取代style-loader 作用: 提取js中的css成单独文件
+                    MiniCssExtractPlugin.loader,
                     // 将css文件变成commonjs模块加载到js中, 里面内容是样式字符串
-                    'css-loader'
+                    'css-loader',
+                    /**
+                     * css兼容性处理: postcss --> postcss-loader postcss-preset-env
+                     * 
+                     * 帮postcss找到package.json中browserslist里面的配置, 通过配置加载指定的兼容性样式
+                     * "browserslist": {
+                            // 开发环境 --> 设置nodejs环境变量: process.env.NODE_ENV = "development"
+                            "development": [
+                            "last 1 chrome version",
+                            "last 1 firefox version",
+                            "last 1 safari version"
+                            ],
+                            // 生产环境: 默认是生产环境
+                            "production": [
+                            ">0.2%",
+                            "not dead",
+                            "not op_mini all"
+                            ]
+                        }
+                     */
+                    // 使用loader的默认配置
+                    // 'postcss-loader',
+                    // 修改loader的配置
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                // postcass的插件
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
                 ]
             },
             {
@@ -98,6 +137,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             // 复制 ./src/index.html 文件到目标路径, 并引入打包输出的所有资源(js,css等)
             template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // 对输出的css文件进行重命名
+            filename: 'css/built.css'
+
         })
     ],
     // 模式
